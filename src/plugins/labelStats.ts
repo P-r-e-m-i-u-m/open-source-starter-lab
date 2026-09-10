4e5240196aa9e31bf1e903c025d52f88c16fedcf
🄂ʏᴇᴅ 🄰ʙᴅᴜʟ 🄰ᴍᴀ🄝 ✧ <amanbaba9404522@gmail.com>
2026-09-10 14:02:48 +0530

    Create labelStats.ts

---
+++ src/plugins/labelStats.ts
@@ -0,0 +1,4 @@
+// TODO: Implement a command that shows which labels have the most open issues right now, to help contributors find popular topics.
+export function labelStats(): void {
+  console.log("Not implemented yet.");
+}
