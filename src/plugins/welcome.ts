31b9781a56db4c61d50191045d045cf93038c9fb
🄂ʏᴇᴅ 🄰ʙᴅᴜʟ 🄰ᴍᴀ🄝 ✧ <amanbaba9404522@gmail.com>
2026-09-10 14:01:27 +0530

    Create welcome.ts

---
+++ src/plugins/welcome.ts
@@ -0,0 +1,4 @@
+// TODO: Implement a command that generates a personalized welcome message for a new contributor based on their first claimed issue.
+export function welcome(): void {
+  console.log("Not implemented yet.");
+}
