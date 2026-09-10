9c251f00ee68b047f17185c00022af9b17fb4079
🄂ʏᴇᴅ 🄰ʙᴅᴜʟ 🄰ᴍᴀ🄝 ✧ <amanbaba9404522@gmail.com>
2026-09-10 14:01:08 +0530

    Create leaderboard.ts

---
+++ src/plugins/leaderboard.ts
@@ -0,0 +1,4 @@
+// TODO: Implement a command that shows the top five contributors by merged PRs in the last 30 days.
+export function leaderboard(): void {
+  console.log("Not implemented yet.");
+}
