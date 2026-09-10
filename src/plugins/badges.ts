1ff82acf16335a8b03364f1e6acf7bc3238d7880
🄂ʏᴇᴅ 🄰ʙᴅᴜʟ 🄰ᴍᴀ🄝 ✧ <amanbaba9404522@gmail.com>
2026-09-10 14:00:24 +0530

    Create badges.ts

---
+++ src/plugins/badges.ts
@@ -0,0 +1,4 @@
+// TODO: Implement a command that shows which contribution badges a person has earned (first PR, five PRs, docs contributor, etc).
+export function badges(): void {
+  console.log("Not implemented yet.");
+}
