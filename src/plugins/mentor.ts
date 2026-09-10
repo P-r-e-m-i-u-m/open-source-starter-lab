e5093bbe5b5c7be9f398d606bb853d0556d7e9db
🄂ʏᴇᴅ 🄰ʙᴅᴜʟ 🄰ᴍᴀ🄝 ✧ <amanbaba9404522@gmail.com>
2026-09-10 14:01:50 +0530

    Create mentor.ts

---
+++ src/plugins/mentor.ts
@@ -0,0 +1,4 @@
+// TODO: Implement a command that pairs a beginner contributor with a second-pr level contributor for guidance.
+export function mentor(): void {
+  console.log("Not implemented yet.");
+}
