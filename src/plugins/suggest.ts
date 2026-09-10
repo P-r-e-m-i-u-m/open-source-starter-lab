e02e85dc8623eb963e6b2887c577c542f4c475f7
🄂ʏᴇᴅ 🄰ʙᴅᴜʟ 🄰ᴍᴀ🄝 ✧ <amanbaba9404522@gmail.com>
2026-09-10 14:00:52 +0530

    Create suggest.ts

---
+++ src/plugins/suggest.ts
@@ -0,0 +1,4 @@
+// TODO: Implement a command that recommends one open issue based on a skill the user types in, reusing the existing issue-fit logic.
+export function suggest(): void {
+  console.log("Not implemented yet.");
+}
